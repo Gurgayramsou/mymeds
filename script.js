10af776f4a6eebe4843f8921de20b5f5601fa7ad
Gurgayramsou <hegderam17@gmail.com>
2022-07-31 20:52:33 +0530

    registration for display logic

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const lgBtn = document.querySelector('#logBtn');
+const formReg = document.querySelector('.regForm');
+
+// login form appears
 lgBtn.addEventListener('click', function () {
   const formLog = document.querySelector('.lgForm');
+  formReg.style.display = 'none';
   formLog.style.display = 'flex';
-  formLog.style.top = '40%';
+  formLog.style.top = '32%';
 });
 
+// registration form appears
+const regFrm = document.querySelector('#regNav');
+regFrm.addEventListener('click', function () {
+  formReg.style.display = 'flex';
+  formReg.style.top = '32%';
+});
