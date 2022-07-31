9710207452f3dd42c17b7f04299de4b93f013ba3
hegderam17@gmail.com <hegderam17@gmail.com>
2022-07-31 12:10:41 +0530

    js file included

---
+++ script.js
@@ -0,0 +1,5 @@
+const lgBtn = document.querySelector('#logBtn');
+lgBtn.addEventListener('click',function(){
+    const formLog =document.getElementsByClassName('lgForm');
+   @type {HTMLElement} 
+})
